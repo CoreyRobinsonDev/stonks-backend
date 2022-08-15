cde102383646d90f81b2d62d0c80d8e58a18a202
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-08-15 06:08:48 -0400

    completed the buy route

--- src/routes/stocks.ts
+++ src/routes/stocks.ts
@@ -16,7 +16,6 @@ const baseUrl = "https://api.polygon.io/";
 router.post("/groupedDailyBars", (req, res) => {
   const { date, isAdjusted, includeOTC } = req.body;
   const apiCall = `${baseUrl}v2/aggs/grouped/locale/us/market/stocks/${date}?adjusted=${isAdjusted}&include_otc=${includeOTC}&apiKey=${apiKey}`;
-
   axios.get(apiCall)
     .then((response) => res.send(response.data))
     .catch((err) => res.status(400).send(err))
@@ -45,7 +44,7 @@ router.post("/tickerDetails", async (req, res) => {
 router.post("/buy", async (req, res) => {
   const currentDate = new Date().toISOString().slice(0, 10).split("-");
   const yesterday = [...currentDate];
-  yesterday[2] = (+yesterday[2] - 2).toString();
+  yesterday[2] = (+yesterday[2] - 3).toString();
   yesterday[1] = (+yesterday[1] - 1).toString();
   const yesterdayDate = new Date(+yesterday[0], +yesterday[1], +yesterday[2]).toISOString().slice(0, 10);
   
@@ -55,14 +54,20 @@ router.post("/buy", async (req, res) => {
   const price = await axios.get(`${baseUrl}v1/open-close/${symbol}/${yesterdayDate}?apiKey=${apiKey}`)
     .then((response) => response.data.close)
     .catch(() => null)
-  if (!price) return res.status(500).send("Error during price lookup");
   const company_name = await axios.get(`${baseUrl}v3/reference/tickers/${symbol}?apiKey=${apiKey}`)
     .then((response) => response.data.results.name)
     .catch(() => null)
-  if (!company_name) return res.status(500).send("Error during company name lookup");
+  
   const db = await dbPromise;
 
-  db.run("INSERT INTO history (user_id, symbol, price, num_shares, transaction_type, time, company_name) VALUES (?, ?, ?, ?, ?, ?, ?)", [user_id, symbol, price, num_shares, transaction_type, time, company_name])
+  const isSymbol = await axios.get(`${baseUrl}v3/reference/tickers/${symbol}?apiKey=${apiKey}`)
+    .then((response) => response.data.results.ticker === symbol)
+  if (!symbol || !isSymbol) return res.status(400).send("Invalid ticker symbol");
+  if (!num_shares) return res.status(400).send("Shares must be in positive integer amounts");
+  if (!price) return res.status(500).send("Error during price lookup");
+  if (!company_name) return res.status(500).send("Error during company name lookup");
 
+  db.run("INSERT INTO history (user_id, symbol, price, num_shares, transaction_type, time, company_name) VALUES (?, ?, ?, ?, ?, ?, ?)", [user_id, symbol, price, parseInt(num_shares), transaction_type, time, company_name])
+  res.send("Success");
   db.close();
 })
