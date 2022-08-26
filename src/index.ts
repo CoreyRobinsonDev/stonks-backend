ad60b5e72c1157256f769e69bc7bf309c1d81583
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-08-26 06:34:41 -0400

    configuring cors

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const dbPromise = open({
 
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: "https://stonks-crd.netlify.app"}));
 app.use(express.json());
 app.use(session({
   secret: "secretcode",
